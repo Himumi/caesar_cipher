767bc444c9af9b40660356a947314c04e3bf7e05
Himumi <erliansyaha@gmail.com>
2024-01-28 02:26:26 +0900

    Refactor - Use reverse method instead of if statement

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -15,13 +15,11 @@ def caesar_cipher(string, number, direction)
       end
     elsif direction == "right"
       if arr[0].include?(item)
-        index = arr[0].find_index(item) + number
-        index = index - 26 if index > 25
-        item = arr[0][index]
+        index = arr[0].reverse.find_index(item) - number
+        item = arr[0].reverse[index]
       elsif arr[1].include?(item)
-        index = arr[1].find_index(item) + number
-        index = index - 26 if index > 25
-        item = arr[1][index]
+        index = arr[1].reverse.find_index(item) - number
+        item = arr[1].reverse[index]
       else
         item
       end
@@ -33,21 +31,37 @@ end
 puts caesar_cipher("Hai, World!", 1, "left")
 puts caesar_cipher("Hai, World!", 1, "right")
 
-def caesar_cipher_ord(string, number)
+def caesar_cipher_ord(string, number, direction)
+  direction = direction.downcase
   string = string.split("").map do |item|
-    if (65..90).include?(item.ord)
-      shifted = item.ord - number
-      shifted = 90 - (64 - shifted) if shifted < 65
-      item = shifted.chr
-    elsif (97..122).include?(item.ord)
-      shifted = item.ord - number
-      shifted = 122 - (96 - shifted) if shifted < 97
-      item = shifted.chr
-    else
-      item
+    if direction == "left"
+      if (65..90).include?(item.ord)
+        shifted = item.ord - number
+        shifted = 90 - (64 - shifted) if shifted < 65
+        item = shifted.chr
+      elsif (97..122).include?(item.ord)
+        shifted = item.ord - number
+        shifted = 122 - (96 - shifted) if shifted < 97
+        item = shifted.chr
+      else
+        item
+      end
+    elsif direction == "right"
+      if (65..90).include?(item.ord)
+        shifted = item.ord + number
+        shifted = 65 + (91 - shifted) if shifted > 90
+        item = shifted.chr
+      elsif (97..122).include?(item.ord)
+        shifted = item.ord + number
+        shifted = 97 + (123 - shifted) if shifted > 122
+        item = shifted.chr
+      else
+        item
+      end
     end
   end
   string.join
 end
 
-puts caesar_cipher_ord("Hai, World!", 1)
+puts caesar_cipher_ord("Hai, World!", 1, "right")
+puts caesar_cipher_ord("zZ", 1, "right")
