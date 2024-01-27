93615ac5b0ce3e57a7026f9161c5f603395d8f68
Himumi <erliansyaha@gmail.com>
2024-01-28 01:53:41 +0900

    Feat - Create right shift direction feature

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,19 +1,34 @@
-def caesar_cipher(string, number)
+def caesar_cipher(string, number, direction)
   arr = [("A".."Z").to_a, ("a".."z").to_a]
-  string = string.split("")
+  direction = direction.downcase
 
-  string = string.map do |item|
-    if arr[0].include?(item)
-      index = arr[0].find_index(item) - number
-      item = arr[0][index]
-    elsif arr[1].include?(item)
-      index = arr[1].find_index(item) - number
-      item = arr[1][index]
-    else
-      item
+  string = string.split("").map! do |item|
+    if direction == "left"
+      if arr[0].include?(item)
+        index = arr[0].find_index(item) - number
+        item = arr[0][index]
+      elsif arr[1].include?(item)
+        index = arr[1].find_index(item) - number
+        item = arr[1][index]
+      else
+        item
+      end
+    elsif direction == "right"
+      if arr[0].include?(item)
+        index = arr[0].find_index(item) + number
+        index = index - 26 if index > 25
+        item = arr[0][index]
+      elsif arr[1].include?(item)
+        index = arr[1].find_index(item) + number
+        index = index - 26 if index > 25
+        item = arr[1][index]
+      else
+        item
+      end
     end
   end
   string.join
 end
 
-puts caesar_cipher("Hai World", 2)
+puts caesar_cipher("Hi, World!", 1, "left")
+puts caesar_cipher("Hi, World!", 1, "right")
